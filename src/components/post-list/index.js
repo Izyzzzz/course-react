9f83b14d63866060c395afc6d555c559487e91f5
Serg <izyzzzz@gmail.com>
2019-01-21 16:41:27 +0200

    lesson-03-add

--- src/components/post-list/index.js
+++ src/components/post-list/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './post-list.css';
 
-import PostListItem from '../post-list-item/post-list-item';
+import PostListItem from '../post-list-item/';
 
 const PostList = () => {
     return (
