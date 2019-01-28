0d7e832147e73abd8da0a60aa3b00f5b17226d86
Serg <izyzzzz@gmail.com>
2019-01-28 14:43:42 +0200

    lesson-07-add no-style

--- src/components/index.js
+++ src/components/index.js
@@ -20,29 +20,32 @@ export default class Base extends Component {
         })
     }
 
-    // newBase = () => {
-    //     if (this.state.newBase) {
-        
-    //         const elements = this.state.newBase.map((item) => {
-    //             return (
-    //                 <li >
-    //                     {item.name}
-    //                 </li>
-    //             )
-    //         });
-    //         return elements;
-            
-    //         }
-    // }
+    newBase = () => {
+        let elements = [];
+        if (this.state.newBase) {
+            for (let key in this.state.newBase) {
+                const items = this.state.newBase[key].map((item) => {
+                    return (
+                        <div key={item.name}>
+                            <p>{item.name}</p>
+                            <p>{item.fruit_category}</p>
+                            <p>{item.country}</p>
+                            <img src={item.img_url} alt=""/>
+                        </div>
+                    )
+                });              
+                elements.push(items);
+                }
+            }
+            return elements;
+    }
 
     render() {
                
                 
       return (
         <div>
-            {this.state.newBase ?
-                this.state.newBase.fruits[0].name : 'No'
-            }
+            {this.newBase()}
         </div>
       );
     }
