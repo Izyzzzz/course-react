c4e48338c4e1f8e41d9f29fc36cf78535d4995f3
Serg <izyzzzz@gmail.com>
2019-01-28 20:11:16 +0200

    lesson-07-extra that all

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,31 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
 import getService from '../services/getService'
+
+
+const Block =styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    max-width: 500px;
+    margin: 30px auto;
+    box-shadow: 0px 0px 15px 10px #999;
+`
+const ItemImg = styled.div`
+    grid-column-start: 2; 
+    grid-column-end: 3; 
+    grid-row-start: 1; 
+    grid-row-end: 4;
+    img {
+        width: 100%;
+    }
+    
+`
+const ItemText = styled.div`
+    display: grid;
+    align-items: center;
+    justify-content: center;
+`
 export default class Base extends Component {
     constructor(props) {
         super(props);
@@ -26,12 +51,12 @@ export default class Base extends Component {
             for (let key in this.state.newBase) {
                 const items = this.state.newBase[key].map((item) => {
                     return (
-                        <div key={item.name}>
-                            <p>{item.name}</p>
-                            <p>{item.fruit_category}</p>
-                            <p>{item.country}</p>
-                            <img src={item.img_url} alt=""/>
-                        </div>
+                        <Block key={item.name}>
+                            <ItemText>Name: {item.name}</ItemText>
+                            <ItemText>Category: {(item.fruit_category) ? [item.fruit_category] : [item.vegetable_category]}</ItemText>
+                            <ItemText>Country: {item.country}</ItemText>
+                            <ItemImg><img src={item.img_url} alt=""/></ItemImg>
+                        </Block>
                     )
                 });              
                 elements.push(items);
