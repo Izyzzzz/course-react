eede2c56869239d73ea8ab73c2ffa3297c883cc2
Serg <izyzzzz@gmail.com>
2019-02-01 15:41:37 +0200

    lesson-11-extra

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,7 +4,7 @@ import Header from '../header';
 import ErrorMessage from '../errorMessage';
 import {CharacterPage, BookPage, HousePage, BooksItem, HomePage} from '../pages';
 import gotService from '../../services/getService';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import './app.css';
 
@@ -37,6 +37,7 @@ export default class App extends Component {
                         <Header />
                     </Container>
                     <Container>
+                    <Switch>
                         <Route path='/' exact component={HomePage} />                        
                         <Route path='/characters' component={CharacterPage} />
                         <Route path='/houses' component={HousePage} />
@@ -45,10 +46,25 @@ export default class App extends Component {
                             ({match}) => {
                                 const {id} = match.params;
                             return <BooksItem bookId={id}/>}
-                        } />      
+                        } />
+                        <Route patn='' exact component={NoMatch} />
+                    </Switch>  
                     </Container>
                 </div>
             </Router>
         );
     }
+
+    
 };
+
+function NoMatch({ location }) {
+    return (
+      <div>
+        <h3 className="title-nomatch">
+          No match for <code> - {location.pathname}</code>
+        </h3>
+        <a href="/" className="button-back d-flex justify-content-center">Back</a>
+      </div>
+    );
+  }
