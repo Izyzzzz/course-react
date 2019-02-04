b4d7214c73c7d090e73211964358a2b0c13f5d1a
Serg <izyzzzz@gmail.com>
2019-02-04 20:18:46 +0200

    lesson-12

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import {createStore} from 'redux';
+
+const reducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'DEC':
+            return state + 1;
+        case "INC":
+            return state - 1;
+        case 'RND':
+            return state = 0;
+        default:
+            return state;
+    }
+}
+const inc = () => ({type: 'INC'});
+const dec = () => ({type: 'DEC'});
+const rnd = () => ({type: 'RND'});
+
+const store = createStore(reducer);
+
+document.getElementById('inc').addEventListener('click', () => {
+    store.dispatch(inc())
+});
+document.getElementById('dec').addEventListener('click', () => {
+    store.dispatch(dec())
+});
+document.getElementById('rnd').addEventListener('click', () => {
+    store.dispatch(rnd())
+});
+
+const update = () => {
+    document.getElementById('counter').textContent = store.getState();
+}
+
+store.subscribe(update);
